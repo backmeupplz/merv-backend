1a2060f3774be0c22123b443bc6e3aab9580e067
Nikita Kolmogorov <backmeupplz@gmail.com>
2025-03-16 14:00:20 -0700

    Prefix Telegram messages with "[merv]" for better identification

--- src/helpers/reportToTelegram.ts
+++ src/helpers/reportToTelegram.ts
@@ -5,7 +5,7 @@ const bot = new Bot(env.TELEGRAM_BOT_TOKEN)
 
 export default async function reportToTelegram(message: string) {
   try {
-    await bot.api.sendMessage(76104711, message)
+    await bot.api.sendMessage(76104711, `[merv]: ${message}`)
   } catch (error) {
     console.error('Error while sending message to telegram', error)
   }
